92cedde010b61a5389598c0a1bf5f3ad7afda6a3
Suraj Thalange <surajthalange18@gmail.com>
2026-09-06 00:33:19 +0530

    Add en language pack and db/install.xml
    Core rejected the plugin with "detectedbrokenplugin / Missing mandatory en
    language pack" during PHPUnit site install. A mod plugin needs both a
    lang/en pack and a db/install.xml table named after the module before
    upgrade_plugins_modules() will accept it.
    
    With these present, init.php completes cleanly and core PHPUnit tests run.

---
+++ lang/en/suddendeath.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English strings for mod_suddendeath.
+ *
+ * @package    mod_suddendeath
+ * @copyright  2026 Suraj
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Sudden Death';
+$string['modulename_help'] = 'The Sudden Death activity is a stub. No functionality yet.';
+$string['modulenameplural'] = 'Sudden Deaths';
+$string['pluginadministration'] = 'Sudden Death administration';
+$string['pluginname'] = 'Sudden Death';
+$string['suddendeath:addinstance'] = 'Add a new Sudden Death activity';
+$string['suddendeath:view'] = 'View Sudden Death activity';
